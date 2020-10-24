d8bbcc32c5cb46c4ba39d01d176acc774bf8c144
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-10-24 11:04:29 +0100

    added time stamp to simulations

--- run_all_simulations.py
+++ run_all_simulations.py
@@ -3,13 +3,21 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
+import pytz
+tz_London = pytz.timezone('Europe/London')
 
-N = 5
-x = np.random.rand(N)
-y = np.random.rand(N)
-
-plt.scatter(x, y, s=20)
+# plots graph with 5 random data points
+x = np.random.rand(5)
+y = np.random.rand(5)
+plt.scatter(x, y, s=30)
 plt.savefig('/share/new_figure.jpg')
+
+# gets th time stamp information
+tz_London = pytz.timezone('Europe/London') 
+datetime_London = datetime.now(tz_London)    
+date = datetime_London.strftime("%D day month year")
+time = datetime_London.strftime("%H:%M:%S hour minute second (UK time)")
 
 with open('/share/README.md', 'w') as readme:
     readme.write('# example_update_of_readme_figures\n')
@@ -17,8 +25,9 @@ with open('/share/README.md', 'w') as readme:
     readme.write('This repository is a minimal working example of an automated benchmarking')
     readme.write('workflow using Github actions.\n')
     readme.write('\n')
+    readme.write('simulation date ' + date)
+    readme.write('simulation time ' + time)
     readme.write('![latest results](https://github.com/Shimwell/example_update_of_readme_figures/blob/main/new_figure.jpg)')
-
 
 
 #os.system('cp new_figure.jpg /share/new_figure.jpg')
